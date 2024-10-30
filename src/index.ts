67e6a59a61c851860f93866c8aa5979bb6922f49
nahidul-fahim <nahidul7562@gmail.com>
2024-10-31 00:03:04 +0600

    post and get command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Learning prisma with typescript")
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    const result = await prisma.post.create({
+        data: {
+            title: "This is post title 2",
+            content: "This is another post content...",
+            authorName: "Nahidul Islam"
+        }
+    });
+
+    console.log(result)
+
+    // get all data from post table
+    // const result = await prisma.post.findMany();
+    // console.log(result);
+}
+
+main();
