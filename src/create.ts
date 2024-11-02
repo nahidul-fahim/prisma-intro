4e74a62bb050d411b02d36ef92e463ac45de81f9
nahidul-fahim <nahidul7562@gmail.com>
2024-11-02 18:15:14 +0600

    create user and profile with relation

--- src/create.ts
+++ src/create.ts
@@ -1,40 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
 
     // create single data
-    const result = await prisma.post.create({ // here 'post' is a database table name and 'create' is a function
+    // const createUser = await prisma.user.create({
+    //     data: {
+    //         username: "user2",
+    //         email: "user2@ph.com",
+    //         role: UserRole.user
+    //     }
+    // });
+
+    // create profile
+    const createProfile = await prisma.profile.create({
         data: {
-            title: "This is post title 2",
-            content: "This is another post content...",
-            authorName: "Nahidul Islam"
+            bio: "This is my unique bio",
+            userId: 1
         }
     });
 
-    // create many data
-    const createMany = await prisma.post.createMany({
-        data: [
-            {
-                title: "This is post title 4",
-                content: "This is another post content...",
-                authorName: "Nahidul Islam"
-            },
-            {
-                title: "This is post title 5",
-                content: "This is another post content...",
-                authorName: "Nahidul Islam"
-            },
-            {
-                title: "This is post title 6",
-                content: "This is another post content...",
-                authorName: "Nahidul Islam"
-            }
-        ]
-    });
+    console.log({ createProfile })
 
-    console.log(createMany)
 }
 
 main();
